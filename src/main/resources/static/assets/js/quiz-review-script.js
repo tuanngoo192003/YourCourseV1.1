88965c9af37360cf48843784fd1b3f883c02b049
Nguyen Huu Tuan <tuannhhe172829@fpt.edu.vn>
2023-07-04 23:17:25 +0700

    Quiz review finished

--- src/main/resources/static/assets/js/quiz-review-script.js
+++ src/main/resources/static/assets/js/quiz-review-script.js
@@ -26,16 +26,23 @@ for (var i = 0; i < quizData.length; i++) {
     questionDiv.appendChild(optionDText);
 
     var chosenAnswerText = document.createElement("p");
+    var correctAnswerText = document.createElement("p");
     for(var j = 0; j < chosenAnswers.length; j++){
         if(chosenAnswers[j].questionID == quizData[i].questionID){
             chosenAnswerText.innerHTML = "<strong>Chosen Answer:</strong> " + chosenAnswers[j].answerContent;
             questionDiv.appendChild(chosenAnswerText);
+            if(chosenAnswers[j].answerContent == quizData[i].correctAnswer){
+                correctAnswerText.innerHTML = "<strong>Correct Answer:</strong> " + quizData[i].correctAnswer;
+                correctAnswerText.style.color = "green";
+                questionDiv.appendChild(correctAnswerText);
+            }
+            else{
+                correctAnswerText.innerHTML = "<strong>Correct Answer:</strong> " + quizData[i].correctAnswer;
+                correctAnswerText.style.color = "red";
+                questionDiv.appendChild(correctAnswerText);
+            }
         }
     }
-
-    var correctAnswerText = document.createElement("p");
-    correctAnswerText.innerHTML = "<strong>Correct Answer:</strong> " + quizData[i].correctAnswer;
-    questionDiv.appendChild(correctAnswerText);
 
     quizContainer.appendChild(questionDiv);
 }
