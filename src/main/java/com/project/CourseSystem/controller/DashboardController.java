bc09e2ce463f6cd88a7ece899b48726283758742
Le Quang Hong Anh <132969178+hank2701@users.noreply.github.com>
2023-07-11 08:15:55 +0700

    Dashboard

---
+++ src/main/java/com/project/CourseSystem/controller/DashboardController.java
@@ -0,0 +1,11 @@
+package com.project.CourseSystem.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+    @GetMapping("/dashboard")
+    public String getDashboard(){return "dashboard";}
+}
---
+++ src/main/resources/static/assets/js/dash-board.js
@@ -0,0 +1,5 @@
+// Nah there's only pain here, what did you expect?
+
+// The charts hittin hard so I wrote em right in the html file so I wont get lost
+
+//   --Hong Anh--
